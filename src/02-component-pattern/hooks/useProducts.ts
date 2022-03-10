012526b5ca3ab26201b8dbd7a05972c31d21a0fd
Ruth Castro <ruth.avelar80@gmail.com>
2022-03-10 17:55:43 -0600

    control props alternativo

--- src/02-component-pattern/hooks/useProducts.ts
+++ src/02-component-pattern/hooks/useProducts.ts
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react"
+import { useEffect, useState } from "react"
 import { Product, onChangeArgs } from '../interfaces/interfaces';
 
 
@@ -11,16 +11,11 @@ interface useProductArgs {
 export const useProducts = ( { onChange, product, value = 0 }: useProductArgs ) => {
     const [counter, setCounter] = useState(value);
     //!!onChange is it is true because if we write !onChange will be false
-    const isControlled = useRef(!!onChange)
+
     
    
     const increaseBy = (value: number) => {
 
-        if(isControlled.current){
-            // if you dont wantto use onChange! add to the if(isControlled.current && onChange)
-            //onChange! menas typescript trust me you will have a value there
-            return onChange!({ count:value, product })
-        }
         const newValue = Math.max( counter + value, 0 )
         setCounter( newValue )
 
--- src/02-component-pattern/hooks/useShoppingCart.ts
+++ src/02-component-pattern/hooks/useShoppingCart.ts
@@ -14,40 +14,29 @@ export const useShoppingCart = () => {
         // funtion SHOULD MATCH { [key:string]: ProductInCart }
         setShoppingCart(oldShoppingCart => {
 
-            const productInCart: ProductInCart = oldShoppingCart[product.id] || {...product, count:0};
+           
+            
 
-            if(Math.max(productInCart.count + count, 0 ) > 0){
-                productInCart.count += count;
-
+            if (count == 0){
+                // console.log('soy menor que 0',count);
+                // desestructure oldShopping object 
+                // toDelete {id: '1', title: 'COFFEE RUTH', img: './coffee-mug.png', count: 1}
+                //oldShoppingCart is saving the object to delete
+                const {[product.id]: toDelete, ...rest} = oldShoppingCart
+                //delete ({...oldShoppingCart}) [product.id]
+                console.log('Delete when 0 toDelete',toDelete);
+                console.log('Delete when 0 rest',{...rest});
                 return{
-                    ...oldShoppingCart,
-                    [product.id]: productInCart
+                    ...rest
                 }
             }
-            const {[product.id]: toDelete, ...rest} = oldShoppingCart;
-            return {...rest};
-            
-
-            // if (count == 0){
-            //     // console.log('soy menor que 0',count);
-            //     // desestructure oldShopping object 
-            //     // toDelete {id: '1', title: 'COFFEE RUTH', img: './coffee-mug.png', count: 1}
-            //     //oldShoppingCart is saving the object to delete
-            //     const {[product.id]: toDelete, ...rest} = oldShoppingCart
-            //     //delete ({...oldShoppingCart}) [product.id]
-            //     console.log('Delete when 0 toDelete',toDelete);
-            //     console.log('Delete when 0 rest',{...rest});
-            //     return{
-            //         ...rest
-            //     }
-            // }
             
             
-            // return{
-            //     ...oldShoppingCart,
-            //     //computed  [product.id]
-            //     [product.id]: {...product, count }
-            // } 
+            return{
+                ...oldShoppingCart,
+                //computed  [product.id]
+                [product.id]: {...product, count }
+            } 
         })
         
     }
